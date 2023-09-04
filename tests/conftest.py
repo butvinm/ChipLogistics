b7f688007492d39681de4b85cb97ef85595665a0
butvinm <butvin.mihail@yandex.ru>
2023-09-04 03:10:12 +0300

    Close event loop on fixture teardown

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,15 +3,18 @@
 
 import asyncio
 from asyncio import AbstractEventLoop
+from typing import Generator
 
 import pytest
 
 
 @pytest.fixture(scope='session')
-def event_loop() -> AbstractEventLoop:
+def event_loop() -> Generator[AbstractEventLoop, None, None]:
     """Create event loop for async tests.
 
-    Returns:
+    Yields:
         Event loop.
     """
-    return asyncio.get_event_loop()
+    loop = asyncio.get_event_loop()
+    yield loop
+    loop.close()
