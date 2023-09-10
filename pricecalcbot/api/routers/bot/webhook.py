8945b877f96bdc5a9899d176a3319b481760cc12
butvinm <butvin.mihail@yandex.ru>
2023-09-10 08:15:22 +0300

    Inject articles service to bot routers

--- pricecalcbot/api/routers/bot/webhook.py
+++ pricecalcbot/api/routers/bot/webhook.py
@@ -8,9 +8,14 @@ from aiogram.types import Update
 from fastapi import APIRouter, Depends, Header, HTTPException, status
 from pydantic import SecretStr
 
-from pricecalcbot.api.routers.bot.deps import get_bot, get_dispatcher
+from pricecalcbot.api.routers.bot.deps import (
+    get_articles_service,
+    get_bot,
+    get_dispatcher,
+)
 from pricecalcbot.bot.handler_result import HandlerResult
 from pricecalcbot.config import get_bot_secret
+from pricecalcbot.core.articles.service import ArticlesService
 
 router = APIRouter(prefix='/webhook')
 
@@ -19,12 +24,13 @@ SecretHeader = Header(alias='X-Telegram-Bot-Api-Secret-Token')
 
 
 @router.post('/')
-async def handle_update(
+async def handle_update(  # noqa: WPS211
     update: Update,
     secret: Annotated[SecretStr, SecretHeader],
     bot: Annotated[Bot, Depends(get_bot)],
     dispatcher: Annotated[Dispatcher, Depends(get_dispatcher)],
     expected_secret: Annotated[str, Depends(get_bot_secret)],
+    service: Annotated[ArticlesService, Depends(get_articles_service)],
 ) -> HandlerResult:
     """Handle telegram update and propagate to aiogram dispatcher.
 
@@ -38,6 +44,7 @@ async def handle_update(
         dispatcher: Aiogram dispatcher instance.
         expected_secret: Secret for request verification. See `config.py`.
         secret: Request secret.
+        service: Articles service.
 
     Raises:
         HTTPException: 401 if secret is invalid.
@@ -51,4 +58,8 @@ async def handle_update(
             status_code=status.HTTP_401_UNAUTHORIZED,
         )
 
-    return await dispatcher.feed_update(bot, update=update)  # type: ignore
+    return await dispatcher.feed_update(  # type: ignore
+        bot,
+        update=update,
+        articles_service=service,
+    )
