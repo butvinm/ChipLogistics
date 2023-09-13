1b38045221cf05fd1938e5f5f85dbff4e79df3e3
butvinm <butvin.mihail@yandex.ru>
2023-09-13 06:25:23 +0300

    Refactor item adding dialog
    - Remove menu button from dialog start
    - Move dialog routes to separate modules in add_item package

---
+++ chip_logistics/bot/routers/calcs/add_item/start.py
@@ -0,0 +1,39 @@
+"""Start item data collecting route."""
+
+
+from aiogram import Router
+from aiogram.fsm.context import FSMContext
+from aiogram.types import CallbackQuery, Message
+
+from chip_logistics.bot.callbacks.calcs import AddItemCallback
+from chip_logistics.bot.filters.extract_message import ExtractMessage
+from chip_logistics.bot.handler_result import HandlerResult, Ok
+from chip_logistics.bot.states.calcs import CalculationsState
+from chip_logistics.bot.views.calcs.add_item import send_item_count_request
+
+router = Router(name='calcs/add_item/start')
+
+
+@router.callback_query(
+    CalculationsState.wait_continuation,
+    AddItemCallback.filter(),
+    ExtractMessage,
+)
+async def start_item_data_dialog(
+    callback_query: CallbackQuery,
+    message: Message,
+    state: FSMContext,
+) -> HandlerResult:
+    """Ask item count.
+
+    Args:
+        callback_query: Callback query.
+        message: Message where query from.
+        state: Current FSM state.
+
+    Returns:
+        Always success.
+    """
+    await send_item_count_request(message)
+    await state.set_state(CalculationsState.wait_item_count)
+    return Ok()
--- chip_logistics/bot/routers/calcs/root.py
+++ chip_logistics/bot/routers/calcs/root.py
@@ -3,17 +3,13 @@
 
 from aiogram import Router
 
-from chip_logistics.bot.routers.calcs import (
-    contact_select,
-    finish,
-    item_data,
-    start,
-)
+from chip_logistics.bot.routers.calcs import contact_select, finish, start
+from chip_logistics.bot.routers.calcs.add_item import root as add_item
 
 router = Router(name='calcs')
 router.include_routers(
     contact_select.router,
     finish.router,
-    item_data.router,
+    add_item.router,
     start.router,
 )
